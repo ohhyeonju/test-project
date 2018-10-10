459874b844a8aab9eb8db63203120acc76ba68ed
ohhyeonju <hyeonju3212@gmail.com>
2018-10-10 22:22:32 +0900

    .

--- project/src/main/java/bitcamp/java106/pms/App.java
+++ project/src/main/java/bitcamp/java106/pms/App.java
@@ -1,139 +1,138 @@
 package bitcamp.java106.pms;
 
-import java.sql.Date;
 import java.util.Scanner;
 
-import bitcamp.java106.pms.controller.BoardController;
-import bitcamp.java106.pms.controller.MemberController;
-import bitcamp.java106.pms.controller.TaskController;
-import bitcamp.java106.pms.controller.TeamController;
-import bitcamp.java106.pms.controller.TeamMemberController;
-import bitcamp.java106.pms.dao.MemberDao;
-import bitcamp.java106.pms.dao.TaskDao;
-import bitcamp.java106.pms.dao.TeamDao;
-import bitcamp.java106.pms.dao.TeamMemberDao;
 import bitcamp.java106.pms.domain.Member;
 import bitcamp.java106.pms.domain.Team;
-import bitcamp.java106.pms.util.Console;
 
 public class App {
-    static Scanner keyScan = new Scanner(System.in);
-    static String option = null;
-    static void onHelp() {
-        System.out.println("팀 등록 명령: team/add");
-        System.out.println("팀 조회 명령: team/list");
-        System.out.println("팀 상세조회 명령: team/view 팀명");
-        System.out.println("회원 등록 명령: member/add");
-        System.out.println("회원 조회 명령: member/list");
-        System.out.println("회원 등록 명령: member/view 아이디");
-        System.out.println("종료: quit");
-    }
-    
-    static void onQuit() {
-        System.out.println("안녕히가세요!");
-    }
-    
-    public static void main(String args[]) {
-        TeamDao teamDao = new TeamDao();
-        MemberDao memberDao = new MemberDao();
-        TaskDao taskDao = new TaskDao();
-        TeamMemberDao teamMemberDao = new TeamMemberDao(); 
+    public static void main(String[] args) {
+        Scanner keyScan = new Scanner(System.in);
         
-        prepareMemberData(memberDao);
-        prepareTeamData(teamDao, teamMemberDao);
-        
-        TeamController teamController = new TeamController(keyScan, teamDao);
-        MemberController memberController = new MemberController(keyScan, memberDao);
-        BoardController boardController = new BoardController(keyScan);
-        TeamMemberController teamMemberController = new TeamMemberController(
-                keyScan, teamDao, memberDao, teamMemberDao);
-        TaskController taskController = new TaskController(keyScan, teamDao, taskDao,
-                teamMemberDao, memberDao);
-        Console.keyScan = keyScan;
+        Team[] teams = new Team[100];
+        int teamIndex = 0;
+        Member[] members = new Member[100];
+        int memberIndex = 0;
         
         while (true) {
-            String[] str = Console.prompt();
-            String menu = str[0];
-            if (str.length == 2) {
-                option = str[1];
-            } else {
-                option = null;
+            System.out.print("명령> ");
+            String[] input = keyScan.nextLine().toLowerCase().split(" ");
+            String menu = input[0];
+            String option = null;
+            if (input.length == 2) {
+                option = input[1];
             }
             
-            if (menu.equals("help")) {
-                onHelp();
-            } else if (menu.equals("quit")) {
-                onQuit();
+            if (menu.equals("quit")) {
+                System.out.println("안녕히 가세요!");
                 break;
-            } else if (menu.startsWith("team/member/")) {
-                teamMemberController.service(menu, option);
-            } else if (menu.startsWith("team/")) {
-                teamController.service(menu, option);
-            } else if (menu.startsWith("member/")) {
-                memberController.service(menu, option);
-            } else if (menu.startsWith("board/")) {
-                boardController.service(menu, option);
-            } else if (menu.startsWith("task/")) {
-                taskController.service(menu, option);
+            } else if (menu.equals("help")) {
+                System.out.println("팀 등록 명령 : team/add");
+                System.out.println("팀 조회 명령 : team/list");
+                System.out.println("팀 상세조회 명령 : team/view 팀명");
+                System.out.println("회원 등록 명령 : member/add");
+                System.out.println("회원 조회 명령 : member/list");
+                System.out.println("회원 상세조회 명령 : member/view 아이디");
+                System.out.println("종료 : quit");
+                System.out.println();
+                continue;
+            } else if(menu.equals("team/add")) {
+                Team team = new Team();
+                
+                System.out.print("팀명? ");
+                team.name = keyScan.nextLine();
+                
+                System.out.print("설명? ");
+                team.description = keyScan.nextLine();
+
+                System.out.print("최대인원? ");
+                team.maxQty = keyScan.nextInt();
+                keyScan.nextLine(); 
+
+                System.out.print("시작일? ");
+                team.startDate = keyScan.nextLine();
+
+                System.out.print("종료일? ");
+                team.endDate = keyScan.nextLine();
+                
+                teams[teamIndex++] = team;
+                
+                System.out.println();
+                continue;
+            } else if (menu.equals("team/list")) {
+                System.out.println("[팀 목록]");
+                for (int i = 0; i < teamIndex; i++) {
+                    System.out.printf("%s, %d, %s ~ %s\n",
+                            teams[i].name, teams[i].maxQty,
+                            teams[i].startDate, teams[i].endDate);
+                }
+                System.out.println();
+                continue;
+            } else if (menu.equals("team/view")) {
+                if (option == null) {
+                    System.out.println("팀명을 입력하시기 바랍니다.");
+                    System.out.println();
+                    continue;
+                }
+                Team team = null;
+                for (int i = 0; i < teamIndex; i++) {
+                    if (option.equals(teams[i].name.toLowerCase())) {
+                        team = teams[i];
+                        break;
+                    }
+                }
+                if (team == null) {
+                    System.out.println("해당 이름의 팀이 없습니다.");
+                } else {
+                    System.out.printf("팀명: %s\n", team.name);
+                    System.out.printf("설명: %s\n", team.description);
+                    System.out.printf("최대인원: %d\n", team.maxQty);
+                    System.out.printf("기간: %s ~ %s\n", 
+                            team.startDate, team.endDate);
+                }
+            } else if (menu.equals("member/add")) {
+                Member member = new Member();
+                
+                System.out.print("아이디? ");
+                member.id = keyScan.nextLine();
+                
+                System.out.print("이메일? ");
+                member.email = keyScan.nextLine();
+                
+                System.out.print("암호? ");
+                member.password = keyScan.nextLine();
+                
+                members[memberIndex++] = member;
+            } else if (menu.equals("member/list")) {
+                for (int i = 0; i < memberIndex; i++) {
+                    System.out.printf("%s, %s, %s\n",
+                            members[i].id, members[i].email, members[i].password);
+                }
+            } else if (menu.equals("member/view")) {
+                if (option == null) {
+                    System.out.println("팀명을 입력하세요");
+                    System.out.println();
+                    continue;
+                }
+                Member member = null;
+                for (int i = 0; i < memberIndex; i++) {
+                    if (option.equals(members[i].id.toLowerCase())) {
+                        member = members[i];
+                        break;
+                    }
+                }
+                if (member == null) {
+                    System.out.println("해당 아이디의 회원이 없습니다.");
+                } else {
+                    System.out.printf("아이디: %s\n", member.id);
+                    System.out.printf("이메일: %s\n", member.email);
+                    System.out.printf("암호: %s\n", member.password);
+                }
             } else {
                 System.out.println("명령어가 올바르지 않습니다.");
             }
-            
             System.out.println();
         }
-    }
-    static void prepareMemberData(MemberDao memberDao) {
-        Member member = new Member();
-        member.setId("aaa");
-        member.setEmail("aaa@test.com");
-        member.setPassword("1111");
-        memberDao.insert(member);
         
-        member = new Member();
-        member.setId("bbb");
-        member.setEmail("bbb@test.com");
-        member.setPassword("1111");
-        memberDao.insert(member);
-        
-        member = new Member();
-        member.setId("ccc");
-        member.setEmail("ccc@test.com");
-        member.setPassword("1111");
-        memberDao.insert(member);
-        
-        member = new Member();
-        member.setId("ddd");
-        member.setEmail("ddd@test.com");
-        member.setPassword("1111");
-        memberDao.insert(member);
-        
-        member = new Member();
-        member.setId("eee");
-        member.setEmail("eee@test.com");
-        member.setPassword("1111");
-        memberDao.insert(member);
-    }
-    
-    static void prepareTeamData(
-            TeamDao teamDao, TeamMemberDao teamMemberDao) {
-        Team team = new Team();
-        team.setName("t1");
-        team.setMaxQty(5);
-        team.setStartDate(Date.valueOf("2018-2-1"));
-        team.setEndDate(Date.valueOf("2018-5-30"));
-        teamDao.insert(team);
-        teamMemberDao.addMember("t1", "aaa");
-        teamMemberDao.addMember("t1", "bbb");
-        teamMemberDao.addMember("t1", "ccc");
-        
-        team = new Team();
-        team.setName("t2");
-        team.setMaxQty(5);
-        team.setStartDate(Date.valueOf("2018-2-1"));
-        team.setEndDate(Date.valueOf("2018-6-30"));
-        teamDao.insert(team);
-        teamMemberDao.addMember("t2", "ccc");
-        teamMemberDao.addMember("t2", "ddd");
-        teamMemberDao.addMember("t2", "eee");
     }
 }
--- project/src/main/java/bitcamp/java106/pms/domain/Team.java
+++ project/src/main/java/bitcamp/java106/pms/domain/Team.java
@@ -1,58 +1,9 @@
 package bitcamp.java106.pms.domain;
 
-import java.sql.Date;
-
 public class Team {
-    private String name;
-    private String description;
-    private int maxQty;
-    private Date startDate;
-    private Date endDate;
-    
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public int getMaxQty() {
-        return maxQty;
-    }
-
-    public void setMaxQty(int maxQty) {
-        this.maxQty = maxQty;
-    }
-
-    public Date getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(Date startDate) {
-        this.startDate = startDate;
-    }
-
-    public Date getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(Date endDate) {
-        this.endDate = endDate;
-    }
-
-    @Override
-    public String toString() {
-        return "Team [name=" + name + ", description=" + description + ", maxQty=" + maxQty + ", startDate=" + startDate
-                + ", endDate=" + endDate + "]";
-    }
-    
+    public String name;
+    public String description;
+    public int maxQty;
+    public String startDate;
+    public String endDate;
 }
