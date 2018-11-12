8942817809c505052514abe274de75ec3c4dbdbe
ohhyeonju <hyeonju3212@gmail.com>
2018-11-12 20:59:41 +0900

    21

--- project/src/main/java/bitcamp/java106/pms/controller/TaskController.java
+++ project/src/main/java/bitcamp/java106/pms/controller/TaskController.java
@@ -11,7 +11,7 @@ import bitcamp.java106.pms.domain.Task;
 import bitcamp.java106.pms.domain.Team;
 import bitcamp.java106.pms.util.Console;
 
-public class TaskController {
+public class TaskController implements Controller {
     
     Scanner keyScan;
     TeamDao teamDao;
